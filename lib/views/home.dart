51bb63e38fc1c17302d0d007d103a374a641ebaf
Nick Tse <nicholashtse@gmail.com>
2023-07-29 12:13:12 -0400

    add logic to fetch, store, and display all to dos

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:todolist/views/new_To_Do_Form.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +11,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List toDos = [];
+
+  // like useEffect hook
+  @override
+  void initState() {
+    super.initState();
+    fetchToDos();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +31,16 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      body: const Center(child: Text("To Do List Content")),
+      body: ListView.builder(
+        itemCount: toDos.length,
+        itemBuilder: (context, index) {
+          final toDo = toDos[index] as Map;
+          return ListTile(
+            title: Text(toDo['title']),
+            subtitle: Text(toDo['description']),
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: navigateToAddToDo,
         label: const Icon(Icons.add),
@@ -34,5 +54,18 @@ class _HomePageState extends State<HomePage> {
       builder: (context) => const NewToDoForm(),
     );
     Navigator.push(context, route);
+  }
+
+  Future<void> fetchToDos() async {
+    const url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final items = json['items'] as List;
+      setState(() {
+        toDos = items;
+      });
+    }
   }
 }
